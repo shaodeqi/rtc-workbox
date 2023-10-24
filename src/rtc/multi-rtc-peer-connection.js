2fe84b829712cbe2e7136421344f8a8979e4859f
deqishao <deqishao@tencent.com>
2023-10-24 16:06:18 +0800

    update

--- src/rtc/multi-rtc-peer-connection.js
+++ src/rtc/multi-rtc-peer-connection.js
@@ -128,7 +128,7 @@ export class MultiRTCPeerConnection extends EventTarget {
         {
           urls: [
             'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302',
+            // 'stun:stun1.l.google.com:19302',
             'stun:stun2.l.google.com:19302',
           ],
         },

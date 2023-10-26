57ed52deccf59c4754c6e27104e72da0f5795ddf
deqishao <deqishao@tencent.com>
2023-10-26 20:53:31 +0800

    update

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,12 @@
 import RTCResourceChannel from './rtc/resource-channel';
 
 const peer = new URLSearchParams(location.search).get('peer');
+const channel = new RTCResourceChannel(peer, 'ola');
 
 export const register = () => {
   if ('serviceWorker' in navigator) {
     // serviceWorker 注册
     window.addEventListener('load', () => {
-      const channel = new RTCResourceChannel(peer, 'ola');
       channel.listen(navigator.serviceWorker);
       navigator.serviceWorker
         .register('/sw.js')

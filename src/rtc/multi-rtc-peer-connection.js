3e521de546e26f997e3f0abc9df5331acef77b2e
deqishao <deqishao@tencent.com>
2023-10-26 21:04:20 +0800

    update

--- src/rtc/multi-rtc-peer-connection.js
+++ src/rtc/multi-rtc-peer-connection.js
@@ -6,8 +6,8 @@ export default class MultiRTCPeerConnection extends EventTarget {
     iceServers: [
       {
         urls: [
-          // 'stun:stun.l.google.com:19302',
-          // 'stun:stun1.l.google.com:19302',
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
           'stun:stun2.l.google.com:19302',
         ],
       },
